578e592db7a8b23983a962556092b2e9a7ea3557
oyama <52624121+yoyama92@users.noreply.github.com>
2022-05-21 20:32:31 +0900

    入力フォームに区分を追加。保存項目に現在時刻も追加

--- src/code.js
+++ src/code.js
@@ -5,6 +5,18 @@ function doGet() {
 
 function doPost(value) {
   const sheet = SpreadsheetApp.getActiveSheet();
+
+  const now = formatDate(new Date());
   //最終行にデータを追加
-  sheet.appendRow([value["name"], value["email"]]);
+  sheet.appendRow([now, value["selected"], value["name"], value["email"]]);
+}
+
+function formatDate(dt) {
+  dt.setTime(dt.getTime() - dt.getTimezoneOffset() * 60 * 1000);
+  const formattedDate = dt
+    .toISOString()
+    .replace("T", " ")
+    .substr(0, 19)
+    .replaceAll("-", "/");
+  return formattedDate;
 }
