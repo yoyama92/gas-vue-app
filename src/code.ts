02c0391074cf05795dbbaf18f6e7f1d2aab6406c
oyama <52624121+yoyama92@users.noreply.github.com>
2022-05-28 19:21:43 +0900

    リファクタリング

--- src/code.ts
+++ src/code.ts
@@ -1,52 +1,130 @@
-function doGet() {
-  const htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
-  return htmlOutput;
+interface FormService {
+  saveValue(value: FormValue): void;
 }
 
-function doPost(value) {
-  const sheet = SpreadsheetApp.getActiveSheet();
+class FormServiceImpl implements FormService {
+  private slackClient: SlackClient;
+  private spreadSheetClient: SpreadsheetClient;
 
-  const now = formatDate(new Date());
-  //最終行にデータを追加
-  sheet.appendRow([now, value["selected"], value["name"], value["email"]]);
+  constructor(slackClient: SlackClient, spreadSheetClient: SpreadsheetClient) {
+    this.slackClient = slackClient;
+    this.spreadSheetClient = spreadSheetClient;
+  }
 
-  const postText = JSON.stringify({
-    "date": now,
-    "selected": value["selected"],
-    "name": value["name"],
-    "email": value["email"]
-  }, null, 4);
+  saveValue(value: FormValue): void {
+    // スプレッドシートにデータを追加
+    this.spreadSheetClient.appendRow(value.toRow());
 
-  postToSlack(postText)
+    // Slackにメッセージを送信
+    this.slackClient.post(value.toMessage());
+  }
 }
 
-function postToSlack(text) {
-  const options = {
-    "method": "post",
-    "contentType": "application/json",
-    "payload": JSON.stringify({
-      "text": `form data\n${text}`
-    }),
-    "muteHttpExceptions": true
-  };
-
-  const url = getSlackUrl();
-  UrlFetchApp.fetch(url, options);
+interface FormValue {
+  toRow(): string[];
+  toMessage(): string;
 }
 
-function getSlackUrl() {
-  const properties = PropertiesService.getScriptProperties();
-  const url = properties.getProperty('WEBHOOK_URL');
+class FormValueImpl implements FormValue {
+  private date: Date;
+  private selected: string;
+  private name: string;
+  private email: string;
 
-  return url;
+  constructor(date: Date, selected: string, name: string, email: string) {
+    this.date = date;
+    this.selected = selected;
+    this.name = name;
+    this.email = email;
+  }
+
+  toRow(): string[] {
+    return [formatDate(this.date), this.selected, this.name, this.email];
+  }
+
+  toMessage(space: number = 4): string {
+    const text: string = JSON.stringify(
+      {
+        date: formatDate(this.date),
+        selected: this.selected,
+        name: this.name,
+        email: this.email,
+      },
+      null,
+      space
+    );
+    return `form data\n${text}`;
+  }
 }
 
-function formatDate(dt) {
+class SpreadsheetClient {
+  private sheet;
+  constructor(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    this.sheet = sheet;
+  }
+
+  appendRow(row: string[]) {
+    this.sheet.appendRow(row);
+  }
+}
+
+class SlackClient {
+  private webhookUrl: string;
+  constructor(webhookUrl: string) {
+    this.webhookUrl = webhookUrl;
+  }
+
+  post(text: string) {
+    const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+      method: "post",
+      contentType: "application/json",
+      payload: JSON.stringify({
+        text: text,
+      }),
+      muteHttpExceptions: true,
+    };
+
+    UrlFetchApp.fetch(this.webhookUrl, options);
+  }
+}
+
+function formatDate(dt: Date) {
   dt.setTime(dt.getTime() - dt.getTimezoneOffset() * 60 * 1000);
   const formattedDate = dt
     .toISOString()
     .replace("T", " ")
-    .substr(0, 19)
+    .substring(0, 19)
     .replaceAll("-", "/");
   return formattedDate;
+}
+
+function getSlackUrl(): string {
+  const properties = PropertiesService.getScriptProperties();
+  const url = properties.getProperty("WEBHOOK_URL") ?? "";
+
+  return url;
+}
+
+/**
+ * main
+ */
+
+const webhookUrl: string = getSlackUrl();
+const slackClient = new SlackClient(webhookUrl);
+
+const sheet = SpreadsheetApp.getActiveSheet();
+const spreadSheetClient = new SpreadsheetClient(sheet);
+
+const serive: FormService = new FormServiceImpl(slackClient, spreadSheetClient);
+
+function doGet(): GoogleAppsScript.HTML.HtmlOutput {
+  const htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
+  return htmlOutput;
+}
+
+function doPost(e: { selected: string; name: string; email: string }) {
+  const now = new Date();
+  const value: FormValue = new FormValueImpl(now, e.email, e.name, e.email);
+
+  serive.saveValue(value);
 }
